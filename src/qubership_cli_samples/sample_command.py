fff5539b520fc32154308c99b472ae6a4e4fbb74
Igor Lebedev <lebedev.light@gmail.com>
2025-06-18 12:15:53 +0300

    Add branch to test 3.9 backport of common lib

--- src/qubership_cli_samples/sample_command.py
+++ src/qubership_cli_samples/sample_command.py
@@ -32,16 +32,16 @@ class CalcCommand(ExecutionCommand):
         self.context.logger.info("Running CalcCommand - calculating operation on 'param_1' and 'param_2'...")
         param1 = int(self.context.input_param_get("params.param_1"))
         param2 = int(self.context.input_param_get("params.param_2"))
-        match self.context.input_param_get("params.operation"):
-            case "add":
-                result = param1 + param2
-            case "subtract":
-                result = param1 - param2
-            case "multiply":
-                result = param1 * param2
-            case "divide":
-                result = param1 / param2
-            case _:
-                raise Exception(f"Invalid operation: {self.context.input_param_get('params.operation')}")
+        op = self.context.input_param_get("params.operation")
+        if op == "add":
+            result = param1 + param2
+        elif op == "subtract":
+            result = param1 - param2
+        elif op == "multiply":
+            result = param1 * param2
+        elif op == "divide":
+            result = param1 / param2
+        else:
+            raise Exception(f"Invalid operation: {op}")
         self.context.output_param_set(f"params.{self.context.input_param_get('params.result_name')}", result)
         self.context.output_params_save()
