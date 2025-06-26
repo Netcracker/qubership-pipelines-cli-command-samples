02eab66e7747f0a34b51e51a0f160e935d7bc56a
Igor Lebedev <lebedev.light@gmail.com>
2025-06-26 09:30:28 +0300

    feat: Update approach to passing context/params in CLI commands

--- src/qubership_cli_samples/__main__.py
+++ src/qubership_cli_samples/__main__.py
@@ -7,75 +7,76 @@ from qubership_cli_samples.report.report_command import BuildReport
 from qubership_cli_samples.sample_command import SampleStandaloneExecutionCommand, CalcCommand
 from qubership_pipelines_common_library.v1.execution.exec_logger import ExecutionLogger
 from qubership_cli_samples.umbrella_test.umbrella_command import UmbrellaCommand
-
-DEFAULT_CONTEXT_FILE_PATH = 'context.yaml'
+from qubership_pipelines_common_library.v1.utils.utils_cli import utils_cli
 
 
 @click.group(chain=True)
 def cli():
-    logging.basicConfig(stream=sys.stdout, format=ExecutionLogger.DEFAULT_FORMAT, level=logging.INFO)
+    #logging.basicConfig(stream=sys.stdout, format=ExecutionLogger.DEFAULT_FORMAT, level=logging.INFO)
     current_path = os.path.dirname(os.path.abspath(__file__))
     sys.path.insert(0, current_path)
     #urllib3.disable_warnings()
 
 
 @cli.command("run-sample")
-@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
-def __run_sample(context_path):
-    command = SampleStandaloneExecutionCommand(context_path)
+@utils_cli
+def __run_sample(**kwargs):
+    command = SampleStandaloneExecutionCommand(**kwargs)
     command.run()
 
 
 @cli.command("calc")
-@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
-def __calc(context_path):
-    command = CalcCommand(context_path)
+@utils_cli
+def __calc(**kwargs):
+    command = CalcCommand(**kwargs)
     command.run()
 
 
 @cli.command("list-minio-files")
-@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
-def __list(context_path):
-    command = ListMinioBucketObjectsCommand(context_path)
+@utils_cli
+def __list(**kwargs):
+    command = ListMinioBucketObjectsCommand(**kwargs)
     command.run()
 
 
 @cli.command("umbrella-test")
-def __devtest():
+@utils_cli
+def __devtest(**kwargs):
     command = UmbrellaCommand(folder_path="./RESULTS_FOLDER", input_params={"systems": {"gitlab": {"url": "https://gitlab.com"}}})
     command.run()
 
 
 @cli.command("download-file")
-@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
-def __download_file(context_path):
-    command = DownloadFileExecutionCommand(context_path)
+@utils_cli
+def __download_file(**kwargs):
+    command = DownloadFileExecutionCommand(**kwargs)
     command.run()
 
 
 @cli.command("analyze-file")
-@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
-def __download_file(context_path):
-    command = AnalyzeFileExecutionCommand(context_path)
+@utils_cli
+def __download_file(**kwargs):
+    command = AnalyzeFileExecutionCommand(**kwargs)
     command.run()
 
 
 @cli.command("generate-context-from-env")
 @click.option('--context_folder', required=True, type=str, help="Path to context folder to create")
-def __generate_context_from_env(context_folder):
+@utils_cli
+def __generate_context_from_env(context_folder, **kwargs):
     command = GenerateContextFromEnv(input_params={"params": {"context_folder": context_folder}})
     command.run()
 
 
 @cli.command("github-run-pipeline")
-@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
-def __trigger_github_pipeline(context_path):
-    command = GithubRunPipeline(context_path)
+@utils_cli
+def __trigger_github_pipeline(**kwargs):
+    command = GithubRunPipeline(**kwargs)
     command.run()
 
 
 @cli.command("generate-html-report")
-@click.option('--context_path', required=True, default=DEFAULT_CONTEXT_FILE_PATH, type=str, help="Path to context")
-def __trigger_github_pipeline(context_path):
-    command = BuildReport(context_path)
+@utils_cli
+def __trigger_github_pipeline(**kwargs):
+    command = BuildReport(**kwargs)
     command.run()
--- tests/cli/test_cli_commands.py
+++ tests/cli/test_cli_commands.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import unittest
 import yaml
+import re
 from pathlib import Path
 
 QUBER_CLI = "qubership_cli_samples"
@@ -18,16 +19,39 @@ class TestSampleCLICommands(unittest.TestCase):
         self.assertTrue("Commands:" in output.stdout + output.stderr)
 
     def test_run_sample(self):
-        output = subprocess.run(["python", QUBER_CLI, "run-sample", "--context_path=./data/context.yaml"], capture_output = True, text = True)
+        output = subprocess.run(["python", QUBER_CLI, "run-sample", "--context_path=./data/context.yaml"],
+                                capture_output = True, text = True)
         with open('./data/result.yaml', 'r', encoding='utf-8') as file:
             result = yaml.safe_load(file)
             self.assertEqual(19,  result['params']['result'])
 
+    def test_run_sample_with_input_params(self):
+        output = subprocess.run(["python", QUBER_CLI, "run-sample", "-p params__param_1=11", "-p params__param_2=11"],
+                                capture_output = True, text = True)
+        match = re.search(r'.*paths.output.params: (.*)', output.stdout)
+        if not match:
+            raise Exception(f"Unexpected command execution result:\n{output.stdout + output.stderr}")
+        with open(match.group(1), 'r', encoding='utf-8') as file:
+            result = yaml.safe_load(file)
+            self.assertEqual(22,  result['params']['result'])
+
     def test_calc_sample(self):
-        output = subprocess.run(["python", QUBER_CLI, "calc", "--context_path=./data/context2.yaml"], capture_output = True, text = True)
+        output = subprocess.run(["python", QUBER_CLI, "calc", "--context_path=./data/context2.yaml"],
+                                capture_output = True, text = True)
         with open('./data/result_calc.yaml', 'r', encoding='utf-8') as file:
             result = yaml.safe_load(file)
             self.assertEqual(0.9,  result['params']['result_divide'])
+
+    def test_calc_sample_with_input_params(self):
+        output = subprocess.run(["python", QUBER_CLI, "calc", "-p params.param_1=9", "-p params.param_2=10",
+                                 "-p params.operation=multiply", "-p params.result_name=result_divide"],
+                                capture_output=True, text=True)
+        match = re.search(r'.*paths.output.params: (.*)', output.stdout)
+        if not match:
+            raise Exception(f"Unexpected command execution result:\n{output.stdout + output.stderr}")
+        with open(match.group(1), 'r', encoding='utf-8') as file:
+            result = yaml.safe_load(file)
+            self.assertEqual(90, result['params']['result_divide'])
 
     # def test_umbrella(self):
     #     subprocess.run(["python", QUBER_CLI, "umbrella-test"])
